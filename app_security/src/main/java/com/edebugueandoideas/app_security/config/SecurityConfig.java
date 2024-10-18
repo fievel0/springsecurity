dda14e421389510b4954146d1e8f744f8d866090
Fabián Paucar <faibol2000@gmail.com>
2024-10-18 13:49:48 -0500

    Primera creación del security

---
+++ app_security/src/main/java/com/edebugueandoideas/app_security/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.edebugueandoideas.app_security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(@org.jetbrains.annotations.NotNull HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Si deseas desactivar CSRF, pero no es recomendable en producción
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/v1/index2").permitAll() // Permitir acceso a esta ruta
+                        .anyRequest().authenticated() // Cualquier otra solicitud debe estar autenticada
+                )
+                .formLogin(form -> form
+                        .permitAll() // Permitir acceso a la página de inicio de sesión
+                )
+                .logout(logout -> logout
+                        .permitAll() // Permitir acceso a todos a la página de cierre de sesión
+                );
+
+        return http.build();
+    }
+}
